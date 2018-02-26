a962a4ac64adbe168dfaa9b73f8d723b55d29a75
YongBeomKim <saltman21@naver.com>
2018-02-26 21:47:51 +0900

    erdos

--- vote_web/mysite/settings.py
+++ vote_web/mysite/settings.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')$ct%pm76(5sk&jy8_164%(lrhc+@vj07ss+isu-#amyff8gr_'
+SECRET_KEY =
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
